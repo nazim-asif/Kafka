7c87bcf2c3d3ead9fdd04a575f7d60f67a56d8e4
nazim-asif <nazim.uddin@brainstation-23.com>
2024-04-09 19:38:52 +0600

    Implement Dlt

--- kafkaconsumer/kafkaconsumer/src/main/java/com/example/kafkaconsumer/service/KafkaMessageListener.java
+++ kafkaconsumer/kafkaconsumer/src/main/java/com/example/kafkaconsumer/service/KafkaMessageListener.java
@@ -1,8 +1,13 @@
 package com.example.kafkaconsumer.service;
 
 import com.example.kafkaconsumer.dto.Customer;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,10 +22,14 @@ public class KafkaMessageListener {
         System.out.println("consumer consume the events {} "+ customer.toString());
     }*/
 
+    @RetryableTopic(attempts = "4",backoff = @Backoff(delay = 3000, multiplier = 1.5, maxDelay = 15000), exclude = { NullPointerException.class})
      @KafkaListener(topics = "event2",groupId = "group-1",
                     topicPartitions = {@TopicPartition(topic = "event2", partitions = {"2"})})
     public void consumeEvents(String message) {
         System.out.println("consumer consume the events "+ message);
+        if (message.equals("null")){
+            throw new RuntimeException("Null pointer Exception!");
+        }
     }
 
     @KafkaListener(topics = "event1", groupId = "group-1")
@@ -35,4 +44,9 @@ public class KafkaMessageListener {
     public void consume3(String message){
         System.out.println("Consumer3 Message: "+ message);
     }*/
+
+    @DltHandler
+    public void listenDLT(String message){
+        System.out.println("DLT Received "+ message);
+    }
 }
